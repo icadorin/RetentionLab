0014f53c56eca6a9dcb0a53ff79b75b74dbf20db
icadorin <cadorinisrael@gmail.com>
2026-02-24 22:43:22 -0300

    First commit

---
+++ src/main/java/io/retentionlab/core/RetentionLabCoreEngineApplication.java
@@ -0,0 +1,13 @@
+package io.retentionlab.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetentionLabCoreEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetentionLabCoreEngineApplication.class, args);
+	}
+
+}
